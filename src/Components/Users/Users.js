ec5f105d28763f5b5c45e7cdc7ff1b396e2d158b
Alexander <alex_tokarev01@mail.ru>
2022-04-02 20:09:08 +0300

    commited-state-in-component
    fix: id follow
    update: render stand in pure component

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,6 +1,58 @@
 import React from "react";
 import styles from "./Users.module.css";
 function Users(props) {
+  if (props.users.length === 0) {
+    props.setUsers([
+      {
+        id: "1",
+        name: "Aleksandr Tokarev",
+        status: "Welcome to the club",
+        location: {
+          country: "Russia",
+          city: "Volgograd",
+        },
+        followed: false,
+        avatar:
+          "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
+      },
+      {
+        id: "2",
+        name: "Dmitriy Tokarev",
+        status: "Welcome",
+        location: {
+          country: "Belgium",
+          city: "Geel",
+        },
+        followed: true,
+        avatar:
+          "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
+      },
+      {
+        id: "3",
+        name: "Dan Tokarev",
+        status: "Welcome to",
+        location: {
+          country: "Russia",
+          city: "Moscow",
+        },
+        followed: true,
+        avatar:
+          "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
+      },
+      {
+        id: "4",
+        name: "Max Tokarev",
+        status: "Welcome to the",
+        location: {
+          country: "Russia",
+          city: "Omsk",
+        },
+        followed: false,
+        avatar:
+          "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
+      },
+    ]);
+  }
   return (
     <div>
       {props.users.map((u) => (
@@ -11,21 +63,11 @@ function Users(props) {
             </div>
             <div>
               {u.followed ? (
-                <button
-                  onClick={() => {
-                    debugger;
-                    props.onToggleFollow(u.id);
-                  }}
-                >
+                <button onClick={() => props.onToggleFollow(u.id)}>
                   FOLLOWED
                 </button>
               ) : (
-                <button
-                  onClick={() => {
-                    debugger;
-                    props.onToggleFollow(u.id);
-                  }}
-                >
+                <button onClick={() => props.onToggleFollow(u.id)}>
                   UNFOLLOWED
                 </button>
               )}
--- src/Redux/usersReducer.js
+++ src/Redux/usersReducer.js
@@ -1,65 +1,14 @@
 import React from "react";
 const IS_FOLLOWED = "IS-FOLLOWED";
 const SET_USERS = "SET-USERS";
-let initialState = {
-  users: [
-    {
-      id: "1",
-      name: "Aleksandr Tokarev",
-      status: "Welcome to the club",
-      location: {
-        country: "Russia",
-        city: "Volgograd",
-      },
-      followed: false,
-      avatar:
-        "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
-    },
-    {
-      id: "2",
-      name: "Dmitriy Tokarev",
-      status: "Welcome",
-      location: {
-        country: "Belgium",
-        city: "Geel",
-      },
-      followed: true,
-      avatar:
-        "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
-    },
-    {
-      id: "3",
-      name: "Dan Tokarev",
-      status: "Welcome to",
-      location: {
-        country: "Russia",
-        city: "Moscow",
-      },
-      followed: true,
-      avatar:
-        "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
-    },
-    {
-      id: "4",
-      name: "Max Tokarev",
-      status: "Welcome to the",
-      location: {
-        country: "Russia",
-        city: "Omsk",
-      },
-      followed: false,
-      avatar:
-        "http://pm1.narvii.com/7501/8f4cc5dbb7c44817825e7980169728b9e80df31fr1-600-900v2_uhq.jpg",
-    },
-  ],
-};
+let initialState = { users: [] };
 function usersReducer(state = initialState, action) {
   switch (action.type) {
     case IS_FOLLOWED: {
       return {
         ...state,
         users: state.users.map((u) => {
-          if (u.userID === action.userID) {
+          if (u.id === action.userID) {
             return { ...u, followed: !u.followed };
           }
           return u;
@@ -67,7 +16,7 @@ function usersReducer(state = initialState, action) {
       };
     }
     case SET_USERS: {
-      return { ...state, users: [...state.users, action.users] };
+      return { ...state, users: [...state.users, ...action.users] };
     }
     default:
       return state;
@@ -78,5 +27,5 @@ export const onToggleFollowActionCreator = (userID) => ({
   type: IS_FOLLOWED,
   userID,
 });
-export const setUsersActionCreator = () => ({ type: SET_USERS });
+export const setUsersActionCreator = (users) => ({ type: SET_USERS, users });
 export default usersReducer;
