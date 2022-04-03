dc33a720dddd53eaa4f05daab0c940d5935a6c43
Alexander <alex_tokarev01@mail.ru>
2022-04-03 07:21:46 +0300

    commit-header-feature
    link to profile from header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <header className={styles.header}>
-      <img src="https://media.discordapp.net/attachments/959904939096834048/960028115395035136/132131231.png"></img>
+      <NavLink
+        to="/profile"
+        className={(navData) =>
+          navData.isActive ? styles.activeLink : styles.disableLink
+        }
+      >
+        <img src="https://media.discordapp.net/attachments/959904939096834048/960028115395035136/132131231.png"></img>
+      </NavLink>
     </header>
   );
 }
